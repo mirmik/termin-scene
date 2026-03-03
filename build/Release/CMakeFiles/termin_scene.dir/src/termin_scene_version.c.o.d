18831be599b3d1dc9a5b01ef1adf064b1d03e6fc
netricks <mirmikns@yandex.ru>
2026-03-03 21:37:45 +0300

    'init'

--- build/Release/CMakeFiles/termin_scene.dir/src/termin_scene_version.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/termin_scene.dir/src/termin_scene_version.c.o: \
- /home/mirmik/project/termin-env/termin-scene/src/termin_scene_version.c \
- /usr/include/stdc-predef.h \
- /home/mirmik/project/termin-env/termin-scene/include/termin_scene/termin_scene.h
